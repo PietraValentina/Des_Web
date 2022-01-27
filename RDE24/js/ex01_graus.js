64dd9fc5c3cff757a4fe9979dd1e49e43207e40f
PietraValentina <pcanesso@gmail.com>
2022-01-27 17:16:12 -0300

    up

---
+++ RDE24/js/ex01_graus.js
@@ -0,0 +1,9 @@
+var c, f, k
+c=prompt("Digite a temperatura em Celsius: ")
+var f = perseInt(c)*1.8+32;
+var k = perseInt(c)+273.15
+alert("A temperatura em Celsius:"+c+"\nA temperatura em Fahrenheit: "+f+"\n A temperatura em Kelvin: "+k)
+//document.getElementById("demo").innerHTML = k
+//document.write("A temperatura em Fahrenheit: "+f)
+//document.write("\nA temperatura em Kelvin: "+k)
+
---
+++ RDE24/js/ex02.js
@@ -0,0 +1,6 @@
+var  num, dolar, real
+const cot=5.45
+
+var dolar=num*cot;
+var real=num/cot;
+alert("O resultado é: "+dolar)
